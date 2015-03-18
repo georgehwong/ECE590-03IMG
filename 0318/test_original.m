b479f448282d94f86b053ce635558b0a687cef46
yifeng huang <nickhuang0810@gmail.com>
2015-03-18 16:31:11 -0400

    result

--- 0318/test_original.m
+++ 0318/test_original.m
@@ -1,4 +1,6 @@
 close all;
 clear all;
-load img62_blur_defocus_Gaussian3.mat;
-[H, img_deblurred] = BlindDeblurring_MichaeliIrani_v1(noiseImg, 51);
+load img62_blur_kernel1.mat;
+[H, img_deblurred] = BlindDeblurring_MichaeliIrani_v1(noiseI, 51);
+imagesc(H);
+colormap(gray);
