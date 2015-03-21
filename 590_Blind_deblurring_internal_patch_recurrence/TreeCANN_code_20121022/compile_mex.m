9e4e27acaeb98b263d12f836c16a22c985c8085f
georgehwong <huangshangru@126.com>
2015-03-21 19:05:38 -0400

    TEST commit

--- 590_Blind_deblurring_internal_patch_recurrence/TreeCANN_code_20121022/compile_mex.m
+++ 590_Blind_deblurring_internal_patch_recurrence/TreeCANN_code_20121022/compile_mex.m
@@ -9,7 +9,7 @@ mex OPTIMFLAGS="/Ox /Oi /Oy /DNDEBUG /fp:fast /arch:SSE2 /DMEX_MODE /openmp"  ./
 
 disp('Creating TreeCANN_extract_patches...');
 %mex OPTIMFLAGS="/Ox /Oi /Oy /DNDEBUG /fp:fast /arch:SSE2 /DMEX_MODE /openmp" -inline ./C_code/TreeCANN.cpp ./C_code/TreeCANN_reduce_patches_mex.cpp -output ./C_code/TreeCANN_reduce_patches
-mex OPTIMFLAGS="/Ox /Oi /Oy /DNDEBUG /fp:fast /arch:SSE2 /DMEX_MODE /openmp"  ./C_code/TreeCANN.cpp ./C_code/TreeCANN_reduce_patches_mex.cpp -output ./C_code/ENN_matching
+mex OPTIMFLAGS="/Ox /Oi /Oy /DNDEBUG /fp:fast /arch:SSE2 /DMEX_MODE /openmp"  ./C_code/TreeCANN.cpp ./C_code/ENN_matching_mex.cpp -output ./C_code/ENN_matching
 
 disp('Creating TreeCANN_propagation_stage...');
 %mex OPTIMFLAGS="/Ox /Oi /Oy /DNDEBUG /fp:fast /arch:SSE2 /DMEX_MODE /openmp" -inline ./C_code/TreeCANN.cpp ./C_code/TreeCANN_propagation_stage_mex.cpp -output ./C_code/TreeCANN_propagation_stage
