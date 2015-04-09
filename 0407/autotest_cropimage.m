a58ce06932422a2d4f820b973ac160bca50e0087
Jujulia <caoyunzhe06@gmail.com>
2015-04-09 11:27:57 -0400

    reslove conflict

---
+++ 0407/autotest_cropimage.m
@@ -0,0 +1,47 @@
+%% load image
+close all;
+clear all;
+
+
+I = imread('1_7_blurred.png');
+crop_deblur;
+save('crop_kernel_1_7_blurred.mat','H');
+
+
+close all;
+clear all;
+
+I = imread('6_1_blurred.png');
+crop_deblur;
+save('crop_kernel_6_1_blurred.mat','H');
+
+
+
+
+close all;
+clear all;
+
+I = imread('6_1_blurred.png');
+crop_deblur;
+save('crop_kernel_6_1_blurred.mat','H');
+
+
+
+
+close all;
+clear all;
+
+I = imread('10_1_blurred.png');
+crop_deblur;
+save('crop_kernel_10_1_blurred.mat','H');
+
+
+
+close all;
+clear all;
+
+I = imread('39_7_blurred.png');
+crop_deblur;
+save('crop_kernel_39_7_blurred.mat','H');
+
+
