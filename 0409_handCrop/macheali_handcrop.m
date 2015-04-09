1af71e560ff28d21f68c69fa409492e31607e793
Jujulia <caoyunzhe06@gmail.com>
2015-04-09 15:40:37 -0400

    add 0409 hand crop img
    那个桥上有赛车的图

---
+++ 0409_handCrop/macheali_handcrop.m
@@ -0,0 +1,14 @@
+clc all;
+clear all;
+blurImg = imread('blurImg.png');
+cropImg = blurImg(165:280,1:290)
+resImg = blurImg;
+resImg(165:280,1:290) = 0;
+[H_crop, img_deblurred_crop] = BlindDeblurringAllLevels(cropImg, 51);
+[H_rest, img_deblurred1_rest] = BlindDeblurringAllLevels(resImg, 51);
+
+save('kernel_handCrop_blurred.mat','H_crop');
+save('kernel_handCrop_blurred.mat','H_rest');
+save('kernel_handCrop_blurred.mat','H_whole');
+save('img_deblurred_handCrop.mat','img_deblurred_crop');
+save('img_deblurred_handCrop.mat','img_deblurred_rest');
